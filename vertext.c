069ffbed35f94be811b8edfd8d2052719100ea1e
im-varun <vrun.mulchandani@gmail.com>
2024-06-03 21:02:05 -0500

    Minor formatting changes

--- vertext.c
+++ vertext.c
@@ -7,21 +7,22 @@
 
 struct termios orig_termios;
 
-void die(const char *s) {
+void die(const char *s){
     perror(s);
     exit(1);
 }
 
-void disableRawMode() {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+void disableRawMode(){
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1){
         die("tcsetattr");
     }
 }
 
-void enableRawMode() {
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+void enableRawMode(){
+    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1){
         die("tcgetattr");
     }
+
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
@@ -33,28 +34,29 @@ void enableRawMode() {
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
 
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1){
         die("tcsetattr");
     }
 }
 
-int main() {
+int main(){
     enableRawMode();
 
-    while (1) {
+    while(1){
         char c = '\0';
 
-        if ((read(STDIN_FILENO, &c , 1) == -1) && (errno != EAGAIN)) {
+        if((read(STDIN_FILENO, &c , 1) == -1) && (errno != EAGAIN)){
             die("read");
         }
 
-        if (iscntrl(c)) {
+        if(iscntrl(c)){
             printf("%d\r\n", c);
-        } else {
+        }
+        else{
             printf("%d ('%c')\r\n", c, c);
         }
         
-        if (c == 'q') {
+        if(c == 'q'){
             break;
         }
     }
