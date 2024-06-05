adb427ca375ff98d6d1ea3bba41090929ea9dbf7
im-varun <vrun.mulchandani@gmail.com>
2024-06-04 19:07:11 -0500

    Add tildes (~) at the start of each line

--- vertext.c
+++ vertext.c
@@ -57,8 +57,18 @@ char editorReadKey(){
     return c;
 }
 
+void editorDrawRows(){
+    for(int i = 0; i < 24; i++){
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
 void editorRefreshScreen(){
     write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editorDrawRows();
+
     write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
