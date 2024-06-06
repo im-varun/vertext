d0da7a9b634d0e8fd0407a8786ef5e759e7c8807
im-varun <vrun.mulchandani@gmail.com>
2024-06-06 16:35:00 -0500

    Display welcome message

--- vertext.c
+++ vertext.c
@@ -7,6 +7,8 @@
 #include <termios.h>
 #include <unistd.h>
 
+#define VERTEXT_VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 struct editorConfig{
@@ -142,7 +144,30 @@ void abFree(struct abuf *ab){
 
 void editorDrawRows(struct abuf *ab){
     for(int i = 0; i < E.screenrows; i++){
-        abAppend(ab, "~", 1);
+        if(i == (E.screenrows / 3)){
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome), "Vertext Editor -- version %s", VERTEXT_VERSION);
+
+            if(welcomelen > E.screencols){
+                welcomelen = E.screencols;
+            }
+
+            int padding = (E.screencols - welcomelen) / 2;
+
+            if(padding){
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+
+            while(padding--){
+                abAppend(ab, " ", 1);
+            }
+
+            abAppend(ab, welcome, welcomelen);
+        }
+        else{
+            abAppend(ab, "~", 1);
+        }
 
         abAppend(ab, "\x1b[K", 3);
         if(i < E.screenrows - 1){
