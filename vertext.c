e08ba669814a6303c825c4f986d7f5e91cde8f60
im-varun <vrun.mulchandani@gmail.com>
2024-06-14 16:01:16 -0500

    Fix bug related to tabs and the cursor

--- vertext.c
+++ vertext.c
@@ -38,6 +38,7 @@ typedef struct erow{
 
 struct editorConfig{
     int cx, cy;
+    int rx;
 
     int rowoff;
     int coloff;
@@ -202,6 +203,20 @@ int getWindowSize(int *rows, int *cols){
     }
 }
 
+int editorRowCxToRx(erow *row, int cx){
+    int rx = 0;
+
+    for(int i = 0; i < cx; i++){
+        if(row->chars[i] == '\t'){
+            rx += (TAB_STOP - 1) - (rx % TAB_STOP);
+        }
+
+        rx++;
+    }
+
+    return rx;
+}
+
 void editorUpdateRow(erow *row){
     int tabs = 0;
     for(int i = 0; i < row->size; i++){
@@ -297,6 +312,11 @@ void abFree(struct abuf *ab){
 }
 
 void editorScroll(){
+    E.rx = 0;
+    if(E.cy < E.numrows){
+        E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+    }
+
     if(E.cy < E.rowoff){
         E.rowoff = E.cy;
     }
@@ -305,12 +325,12 @@ void editorScroll(){
         E.rowoff = E.cy - E.screenrows + 1;
     }
 
-    if(E.cx < E.coloff){
-        E.coloff = E.cx;
+    if(E.rx < E.coloff){
+        E.coloff = E.rx;
     }
 
-    if(E.cx >= E.coloff + E.screencols){
-        E.coloff = E.cx - E.screencols + 1;
+    if(E.rx >= E.coloff + E.screencols){
+        E.coloff = E.rx - E.screencols + 1;
     }
 }
 
@@ -351,7 +371,7 @@ void editorRefreshScreen(){
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);
     abAppend(&ab, buf, strlen(buf));
 
     abAppend(&ab, "\x1b[?25h", 6);
@@ -445,6 +465,7 @@ void editorProcessKeypress(){
 void initEditor(){
     E.cx = 0;
     E.cy = 0;
+    E.rx = 0;
     E.rowoff = 0;
     E.coloff = 0;
     E.numrows = 0;
