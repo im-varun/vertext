63097260625bcd0b161519159f3b4d74af90f2cb
im-varun <vrun.mulchandani@gmail.com>
2024-06-15 19:38:52 -0500

    Add save as functionality to allow user to save a new file

--- vertext.c
+++ vertext.c
@@ -67,6 +67,8 @@ struct editorConfig{
 struct editorConfig E;
 
 void editorSetStatusMessage(const char *fmt, ...);
+void editorRefreshScreen();
+char *editorPrompt(char *prompt);
 
 void die(const char *s){
     write(STDOUT_FILENO, "\x1b[2J", 4);
@@ -450,7 +452,13 @@ void editorOpen(char *filename){
 
 void editorSave(){
     if(E.filename == NULL){
-        return;
+        E.filename = editorPrompt("Save as: %s");
+
+        if(E.filename == NULL){
+            editorSetStatusMessage("Save aborted");
+
+            return;
+        }
     }
 
     int len;
@@ -626,6 +634,50 @@ void editorSetStatusMessage(const char *fmt, ...){
     va_end(ap);
 
     E.statusmsg_time = time(NULL);
+}
+
+char *editorPrompt(char *prompt){
+    size_t bufsize = 128;
+    char *buf = malloc(bufsize);
+
+    size_t buflen = 0;
+    buf[0] = '\0';
+
+    while(1){
+        editorSetStatusMessage(prompt, buf);
+        editorRefreshScreen();
+
+        int c = editorReadKey();
+
+        if((c == DEL_KEY) || (c == CTRL_KEY('h')) || (c == BACKSPACE)){
+            if(buflen != 0){
+                buf[--buflen] = '\0';
+            }
+        }
+        else if(c == '\x1b'){
+            editorSetStatusMessage("");
+            
+            free(buf);
+
+            return NULL;
+        }
+        else if(c == '\r'){
+            if(buflen != 0){
+                editorSetStatusMessage("");
+                
+                return buf;
+            }
+        }
+        else if((!iscntrl(c)) && (c < 128)){
+            if(buflen == (bufsize - 1)){
+                bufsize *= 2;
+                buf = realloc(buf, bufsize);
+            }
+
+            buf[buflen++] = c;
+            buf[buflen] = '\0';
+        }
+    }
 }
 
 void editorMoveCursor(int key){
